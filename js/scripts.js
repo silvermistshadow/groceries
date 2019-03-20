8bd21b3bef0ee512b4c2152392a0ed032515df66
silvermistshadow <bmead72@gmail.com>
2019-03-20 09:43:41 -0700

    start writing javascript and html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+    var inputItems = $("input#itemList").val();
+    var items = inputItems.split(" ");
+
+    
+
+  });
+
+
+
+});
