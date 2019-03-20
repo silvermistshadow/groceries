39ddd74375eeaa0d894429af658f4a0cd393b318
silvermistshadow <bmead72@gmail.com>
2019-03-20 10:09:36 -0700

    finish html and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
+    $("#listOut").empty();
+    $("#listOut").prepend("<ul id='outputList'> </ul>")
     var inputItems = $("input#itemList").val();
     var items = inputItems.split(" ");
 
-    
+    items.sort();
+
+    var outputItems = items.map(function(item){
+      return item.toUpperCase();
+    });
+
+    outputItems.forEach(function(outputItem){
+      $("#outputList").append("<li>" + outputItem + "</li>")
+    });
+
+    $("#listOut").show();
 
   });
 
